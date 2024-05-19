1dfefa7b29c030258307e58cbb7d52e7b0efe45f
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-05-20 00:24:16 +0900

    code update.

--- TestArea/TestArea.ino
+++ TestArea/TestArea.ino
@@ -1,15 +1,14 @@
-#include <DFRobot_DHT11.h> // 이 라이브러리를 사용하겠다. 
-DFRobot_DHT11 DHT;
-#define DHT11_PIN 7
+
 
 void setup(){
-  Serial.begin(9600); // UART 통신 시적
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  digitalWrite(11, HIGH);
 }
 
 void loop(){
-  DHT.read(DHT11_PIN);
-  Serial.print(DHT.temperature); //온도를 출력하겠다.
-  Serial.print(",");
-  Serial.println(DHT.humidity); //습도를 출력하겠다.
+  digitalWrite(10, HIGH);
+  delay(1000);
+  digitalWrite(10, LOW);
   delay(1000);
 }
