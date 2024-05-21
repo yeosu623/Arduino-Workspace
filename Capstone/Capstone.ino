638bd1c5dbfa9a7e205d2ec0eca57bb916c5fc07
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-05-22 00:46:18 +0900

    code update.

--- Capstone/Capstone.ino
+++ Capstone/Capstone.ino
@@ -8,27 +8,41 @@
 #define BUTTON4 5
 #define BUTTON5 6
 #define TEMP_HUMID_SENSOR 7
+#define SPRINGCOOLER1 8
+#define SPRINGCOOLER2 9
+#define FAN 10
+#define LED 11
 #define LIGHT_SENSOR 0
+bool humid_start = false;
+char str1[16], str2[16];
 int button1 = HIGH, button1_prev = HIGH;
 int button2 = HIGH, button2_prev = HIGH;
 int button3 = HIGH, button3_prev = HIGH;
 int button4 = HIGH, button4_prev = HIGH;
 int button5 = HIGH, button5_prev = HIGH;
 int light = 0; // 밝을때 50, 어두울때 10
+int temp_min = 0, temp_max = 30;
+int light_min = 10;
+int hydrate_min = 30, hydrate_max = 50;
+uint64_t preTime, nowTime;
 DFRobot_DHT11 dht;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-int second_clock = 0;
+typedef enum _Mode {
+  TEMP,
+  HUMID,
+  LIGHT,
+  HYDRATE
+} Mode;
+Mode mode = TEMP;
 
 void setup() {
+  preTime = millis();
   Serial.begin(9600);
 
   lcd.begin();
   lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("Smart Farm v1.0");
-  lcd.setCursor(0, 1);
-  lcd.print("Press button");
 
   pinMode(BUTTON1, INPUT_PULLUP);
   pinMode(BUTTON2, INPUT_PULLUP);
@@ -38,37 +52,58 @@ void setup() {
 }
 
 void loop() {
+  nowTime = millis();
   sensors_read();
+  lcd_print();
+  temp_min_check();
+  temp_max_check();
+  light_min_check();
+  hydrate_min_check();
 
   if(button1_push_check()) { // button1 누를 때
     button1_prev = LOW;
-
-    lcd.setCursor(0, 1);
-    lcd.print("button1         ");
+    switch(mode) {
+      case TEMP    : temp_min -= 1; break;
+      case HUMID   : break;
+      case LIGHT   : light_min -= 5; break;
+      case HYDRATE : hydrate_min -= 5; break;
+    }
   }
   if(button2_push_check()) { // button2 누를 때
     button2_prev = LOW;
-
-    lcd.setCursor(0, 1);
-    lcd.print("button2         ");
+    switch(mode) {
+      case TEMP    : temp_min += 1; break;
+      case HUMID   : break;
+      case LIGHT   : light_min += 5; break;
+      case HYDRATE : hydrate_min += 5; break;
+    }
   }
   if(button3_push_check()) { // button3 누를 때
     button3_prev = LOW;
-
-    lcd.setCursor(0, 1);
-    lcd.print("button3         ");
+    switch(mode) {
+      case TEMP    : mode = HUMID;   break;
+      case HUMID   : mode = LIGHT;   break;
+      case LIGHT   : mode = HYDRATE; break;
+      case HYDRATE : mode = TEMP;    break;
+    }
   }
   if(button4_push_check()) { // button4 누를 때
     button4_prev = LOW;
-
-    lcd.setCursor(0, 1);
-    lcd.print("button4         ");
+    switch(mode) {
+      case TEMP    : temp_max -= 1; break;
+      case HUMID   : break;
+      case LIGHT   : break;
+      case HYDRATE : hydrate_max -= 5; break;
+    }
   }
   if(button5_push_check()) { // button5 누를 때
     button5_prev = LOW;
-
-    lcd.setCursor(0, 1);
-    lcd.print("button5         ");
+    switch(mode) {
+      case TEMP    : temp_max += 1; break;
+      case HUMID   : break;
+      case LIGHT   : break;
+      case HYDRATE : hydrate_max += 5; break;
+    }
   }
 
   buttons_pull_check();
@@ -81,8 +116,79 @@ void sensors_read() {
   button4 = digitalRead(BUTTON4);
   button5 = digitalRead(BUTTON5);
 
-  light = analogRead(LIGHT_SENSOR);
-  //dht.read(TEMP_HUMID_SENSOR);
+  if(nowTime - preTime >= 1000) {
+    preTime = nowTime;
+
+    light = analogRead(LIGHT_SENSOR);
+    dht.read(TEMP_HUMID_SENSOR);
+  }
+}
+
+void lcd_print() {
+  switch(mode) {
+    case TEMP :
+      sprintf(str1, "TEMPURATURE : %-2d", dht.temperature);
+      lcd.setCursor(0, 0);
+      lcd.print(str1);
+
+      sprintf(str2, "MIN: %-2d, MAX: %-2d", temp_min, temp_max);
+      lcd.setCursor(0, 1);
+      lcd.print(str2);
+      break;
+
+    case HUMID :
+      if(humid_start == false) {
+        humid_start = true;
+        
+        lcd.setCursor(0, 0);
+        lcd.print("HUMID : ...     ");
+      }
+      if(dht.humidity != 0) {
+        sprintf(str1, "HUMID : %-2d      ", dht.humidity);
+        lcd.setCursor(0, 0);
+        lcd.print(str1);
+      }
+
+      sprintf(str2, "                ");
+      lcd.setCursor(0, 1);
+      lcd.print(str2);
+      break;
+
+    case LIGHT :
+      humid_start = false;
+
+      sprintf(str1, "LIGHT : %-2d      ", light);
+      lcd.setCursor(0, 0);
+      lcd.print(str1);
+
+      sprintf(str2, "MIN: %-2d         ", light_min);
+      lcd.setCursor(0, 1);
+      lcd.print(str2);
+      break;
+
+    case HYDRATE :
+      sprintf(str1, "HYDRATE : %-2d    ", 0);
+      lcd.setCursor(0, 0);
+      lcd.print(str1);
+
+      sprintf(str2, "MIN: %-2d, MAX: %-2d", hydrate_min, hydrate_max);
+      lcd.setCursor(0, 1);
+      lcd.print(str2);
+      break;
+  }
+}
+
+void temp_min_check() {
+
+}
+void temp_max_check() {
+
+}
+void light_min_check() {
+  
+}
+void hydrate_min_check() {
+
 }
 
 int button1_push_check() {
--- TestArea/TestArea.ino
+++ TestArea/TestArea.ino
@@ -1,14 +1,40 @@
+#include <Servo.h>
+#include <LiquidCrystal_I2C.h>
+#define FAN 10 
+#define LED 11
+#define MOTOR1 8
+#define MOTOR2 9
 
+Servo sv1;
+Servo sv2;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-void setup(){
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  digitalWrite(11, HIGH);
+void setup() {
+  // // put your setup code here, to run once:
+  // pinMode(FAN, OUTPUT);
+  // pinMode(LED, OUTPUT);
+  // // digitalWrite(FAN, HIGH);
+  // // digitalWrite(LED, HIGH);
+  // sv1.attach(8);
+  // sv2.attach(9);
+
+  // lcd.begin();
+  // lcd.noBacklight();
+  // lcd.setCursor(0, 0);
+  // lcd.print("Smart Farm v1.0");
 }
 
-void loop(){
-  digitalWrite(10, HIGH);
-  delay(1000);
-  digitalWrite(10, LOW);
-  delay(1000);
+void loop() {
+  // int angle = 0;
+
+  // for(angle = 0; angle < 180; angle++) {
+  //   sv1.write(angle);
+  //   sv2.write(angle);
+  //   delay(20);
+  // }
+  // for(angle = 180; angle > 0; angle--) {
+  //   sv1.write(angle);
+  //   sv2.write(angle);
+  //   delay(20);
+  // }
 }
